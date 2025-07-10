5c5906e5f5634b17bd867044ba0cc7b8c7de5841
Your Name <julian.becker123gmail.com>
2025-07-10 07:20:11 +0200

    Add: animated cursor JS

---
+++ assets/js/animated-cursor.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("mousemove", e => {
+  const cursor = document.getElementById("cursor");
+  cursor.style.top = e.clientY + "px";
+  cursor.style.left = e.clientX + "px";
+});
