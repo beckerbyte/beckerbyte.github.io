d6593aeff9a89d731b4978ccaf530154b15a9797
beckerbyte <julian.becker123@gmail.com>
2026-07-23 14:10:18 +0200

    Verwaiste Altdatei entfernen

--- assets/js/animated-cursor.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("mousemove", e => {
-  const cursor = document.getElementById("cursor");
-  cursor.style.top = e.clientY + "px";
-  cursor.style.left = e.clientX + "px";
-});
